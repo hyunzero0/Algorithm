eaea72ea865ec98a730b8c8ddf05bf177c057548
김현영 <128963113+hyunzero0@users.noreply.github.com>
2023-11-28 00:14:10 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String[] arr = Integer.toString(n).split("");
+
+        for(String num : arr) {
+            answer += Integer.parseInt(num);
+        }
+        
+        return answer;
+    }
+}
